611dc95fa1dbdbf02af3e537804d1c4d021f5ff8
Guilherme Martins <guilhermemspiandorin@gmail.com>
2023-11-27 18:54:40 -0300

    Github pages - Atualizando link header

--- scripts/header_footer.js
+++ scripts/header_footer.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var header = document.createElement("header");
     header.innerHTML = `
     <div id="header">
-        <a href="/" id="logo_header">
+        <a href="index.html" id="logo_header">
             <img src="./assets/Logo Accenture.png" alt="Logo Accenture">
         </a>
         <form id="barra_pesquisa">
